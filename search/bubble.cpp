7243c005e6193c169e2530f152132add673a2a63
Tahsin-Ahmed-Turja <109985922+Tahsin-Ahmed-Turja@users.noreply.github.com>
2023-03-24 00:36:56 +0600

    Create bubble.cpp

---
+++ search/bubble.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubblesl(vector<int>&v,int n){
+    for(int i=0;i<n;i++){
+        bool f = 1;
+        for(int j = i; j<n-1;j++){
+            if(v[j]>v[j+1]){
+                swap(v[j],v[j+1]);
+                f = 0;
+            }
+        }
+        if(f) break;
+    }
+}
+
+void print(vector<int>&v){
+    int n = v.size();
+    for(int i=0;i<n;i++){
+       cout << v[i] <<" ";
+    }
+    cout << endl;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> v = {1,5,2,3,4,6,8};
+    bubblesl(v,v.size());
+    print(v);
+}
