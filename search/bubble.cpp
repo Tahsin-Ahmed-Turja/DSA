109115fc8eb3a673118d3e44b7d4fab45a0d1dc3
Tahsin-Ahmed-Turja <109985922+Tahsin-Ahmed-Turja@users.noreply.github.com>
2023-03-24 01:18:04 +0600

    Update bubble.cpp

--- search/bubble.cpp
+++ search/bubble.cpp
@@ -1,7 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void bubblesl(vector<int>&v,int n){
+void print(vector<int>&v){
+    int n = v.size();
+    for(int i=0;i<n;i++){
+       cout << v[i] <<" ";
+    }
+    cout << endl;
+}
+
+void bubblel(vector<int>&v,int n){
     for(int i=0;i<n;i++){
         bool f = 1;
         for(int j = i; j<n-1;j++){
@@ -14,12 +22,17 @@ void bubblesl(vector<int>&v,int n){
     }
 }
 
-void print(vector<int>&v){
-    int n = v.size();
-    for(int i=0;i<n;i++){
-       cout << v[i] <<" ";
+void bubbleb(vector<int>&v,int n){
+    int r = 0;
+    for(int i = 0;i<n;i++){
+        for(int j =n-2; j>=r;j--){
+            if(v[j]>v[j+1]){
+                swap(v[j],v[j+1]);
+            }
+        }
+        print(v);
+        r++;
     }
-    cout << endl;
 }
 
 int main(){
