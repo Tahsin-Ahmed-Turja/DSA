d44f00c0495e17e2edaa9f6bd6fb90157d455039
Tahsin-Ahmed-Turja <109985922+Tahsin-Ahmed-Turja@users.noreply.github.com>
2023-03-25 01:48:37 +0600

    Create stackofinteger.cpp

---
+++ Stack/stackofinteger.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class mystack
+{
+public:
+    int *a;
+    int size;
+    int capacity;
+    int tos;
+
+    mystack(int cap)
+    {
+        a = new int[cap];
+        capacity = cap;
+        tos = -1;
+        size = 0;
+    }
+
+    void push(int x)
+    {
+        if (size != capacity)
+        {
+            a[++tos] = x;
+            size++;
+        }
+    }
+
+    int pop()
+    {
+        return a[tos--];
+        size--;
+    }
+
+    int top()
+    {
+        return a[tos];
+    }
+
+    bool isempty()
+    {
+        return size == 0;
+    }
+    bool isfull()
+    {
+        return size == capacity;
+    }
+    int getsize(){
+        return size;
+    }
+};
+
+int main(){
+    mystack s = mystack(10);
+    s.push(10);
+    s.push(19);
+    s.push(11);
+    cout << s.top() << endl;
+    cout << s.pop() <<endl;
+    cout << s.top() << endl;
+    cout << s.isempty() << endl;
+}
